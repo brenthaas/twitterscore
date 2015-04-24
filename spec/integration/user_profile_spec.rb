9f51aefd99ee3fb145e89e21f14d612443830c52
Brent Haas <brenthaas@gmail.com>
2015-04-24 14:59:44 -0700

    Created integration specs

---
+++ spec/integration/user_profile_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "Getting a user profile" do
+  let(:handle) { 'loquie' }
+  let(:fields) { %w(name screen_name location) }
+
+  it "responds with all necessary fields" do
+    VCR.use_cassette("get user #{handle}") do
+      get "/users/#{handle}/profile"
+    end
+
+    fields.each do |field|
+      expect(json_response_body.keys).to include(field)
+    end
+  end
+end
