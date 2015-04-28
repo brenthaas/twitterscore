01fa50ae1b2c7b9885653f69d0cf30873718cb7c
Brent Haas <brenthaas@gmail.com>
2015-04-27 17:06:28 -0700

    Add media urls to response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
     if params[:min_retweets].present?
       timeline = filter_by_retweet_count(timeline, params[:min_retweets].to_i)
     end
-    render json: timeline, only: [:created_at, :text, :retweet_count]
+    formatted_hash = format_timeline(timeline)
+    render json: formatted_hash
   end
 
   def score
@@ -24,6 +25,22 @@ class UsersController < ApplicationController
 
   def handle
     params.require(:handle)
+  end
+
+  def format_timeline(timeline)
+    fields = %i(created_at text retweet_count)
+    timeline.map do |post|
+      pruned = post.to_h.select {|key,_| fields.include? key }
+      pruned[:media_urls] = if post.media
+        post.media.map do |m|
+          url = m.media_url
+          url.scheme + "://" + url.host + url.path
+        end
+      else
+        []
+      end
+      pruned
+    end
   end
 
   def filter_by_retweet_count(tweets, min_retweets)
